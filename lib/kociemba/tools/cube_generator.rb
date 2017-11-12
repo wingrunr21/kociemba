26723bac51d96535dd39e002f49227f780f791d8
Stafford Brunk <wingrunr21@gmail.com>
2017-11-12 14:57:12 -0700

    Implement the random cube generator

--- lib/kociemba/tools/cube_generator.rb
+++ lib/kociemba/tools/cube_generator.rb
@@ -3,13 +3,23 @@ require 'kociemba/cubie_cube'
 module Kociemba
   module Tools
     class CubeGenerator
+      CORNER_PERMUTATIONS = 40_320 # 8!
+      EDGE_PERMUTATIONS = 479_001_600 # 12!
+
       class << self
         def random_cube
-          cube = CubieCube.new
-          cube.setFlip()
-          cube.setTwist(rand(26))
+          cc = CubieCube.new
+          cc.flip = rand(Cache::FlipMove::N_FLIP - 1)
+          cc.twist = rand(Cache::TwistMove::N_TWIST - 1)
 
-          cube.to_s
+          loop do
+            cc.urf_to_dlb = rand(CORNER_PERMUTATIONS)
+            cc.ur_to_br = rand(EDGE_PERMUTATIONS)
+
+            break if cc.edge_parity ^ cc.corner_parity == 0
+          end
+
+          cc.to_face_cube.to_s
         end
 
         def random_last_layer_cube
